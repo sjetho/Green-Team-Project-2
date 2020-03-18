ba379d3eb1fc647caef089dd4b0c5111db607427
Nick <nickclay@live.com>
2020-03-17 22:29:06 -0500

    nick

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+// let auth = require("./auth");
+
+// let db = require("./models");
+// let sessions = require("express-session");
+// let cookieParser = require("cookie-parser");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+
+app.use(require("./routes/index"));
+
+app.get("/error", (req, res) => {
+  res.send("error");
+});
+
+app.listen(3500, () => {
+  console.log("Listening on 3500");
+});
