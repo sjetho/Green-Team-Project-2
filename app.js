10bbcd3904fd393035f34627e605900031199319
Nick <nickclay@live.com>
2020-03-19 15:21:24 -0500

    nick

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
+// var io = require('socket.io')();
+
+// const bodyParser = require("body-parser");
 // let auth = require("./auth");
 
-// let db = require("./models");
+// let db = require("./models/data.js");
 // let sessions = require("express-session");
 // let cookieParser = require("cookie-parser");
 
@@ -16,9 +18,19 @@ app.use(require("./routes/current"));
 app.use(require("./routes/future"));
 app.use(require("./routes/donate"));
 
-app.get("/error", (req, res) => {
-  res.send("error");
-});
+// app.get("/error", (req, res) => {
+//   res.send("error");
+// });
+
+
+// io.attach(server);
+// io.on('connection', function (socket) {
+//   socket.on('postMessage', function (data) {
+//     io.emit('updateMessages', data);
+//   });
+// });
+
+
 
 app.listen(3500, () => {
   console.log("Listening on 3500");
