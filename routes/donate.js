434fd2bcca81fab84b4aed98fd0138ca58a12f42
Nick <nickclay@live.com>
2020-03-22 17:16:41 -0500

    nick

--- routes/donate.js
+++ routes/donate.js
@@ -1,8 +1,48 @@
 const express = require('express');
 const router = express.Router();
-router.get('/donate', (req, res) => {
-    res.render('donate', {
-        pageTitle: "donate"
-    });
+// router.get('/donate', (req, res) => {
+//     res.render('donate', {
+//         pageTitle: "donate"
+//     });
+// });
+// module.exports = router;
+
+
+
+
+
+let bodyParser = require("body-parser");
+
+router.use(bodyParser.urlencoded({ extended: false }));
+
+router.get("/donate", (req, res) => {
+    // res.render('donate');
+    db.query('SELECT * FROM "Donations"')
+        .then(results => {
+            //results is an array of objects
+            console.log(results);
+            res.render("donate", {
+                //render donate page
+                Donations: results
+            });
+        });
 });
-module.exports = router;
+
+router.post("/donate", (req, res) => {
+    let UserID = req.body.UserID;
+    let amount_donated = req.body.amount_donated;
+    let body = req.body.content;
+    let item_donated = req.body.item_donated;
+
+    db.none(
+        "INSERT INTO list(Itemname, category, body, imgURL, Amount,Description) VALUES($1, $2, $3, $4)",
+        [UserID, amount_donated, body, item_donated]
+    )
+    res.send('good sent')
+        .then(() => {
+            res.redirect("/donate");
+        })
+        .catch(err => {
+            res.send(err);
+        });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,58 @@
 const express = require('express');
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.render('index', {
-    pageTitle: "index"
-  });
+// router.get('/', (req, res) => {
+//   res.render('index', {
+//     pageTitle: "index"
+//   });
+// });
+
+
+
+
+
+
+let bodyParser = require("body-parser");
+
+router.use(bodyParser.urlencoded({ extended: false }));
+
+router.get("/", (req, res) => {
+  // res.render('current');
+  db.query('SELECT * FROM "Users"')
+    .then(results => {
+      //results is an array of objects
+      console.log(results);
+      res.render("", {
+        //render current page
+        Users: results
+      });
+    });
 });
+
+router.post("/", (req, res) => {
+  let name = req.body.name;
+  let username = req.body.username;
+  let body = req.body.content;
+  let password = req.body.password;
+  let address = req.body.address;
+  db.none(
+    "INSERT INTO Users(name, username, body, password, address) VALUES($1, $2, $3, $4, $5)",
+    [name, username, body, password, address]
+  )
+  res.send('good sent')
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+
+
+
+
+
+
+
+
+
 module.exports = router;
